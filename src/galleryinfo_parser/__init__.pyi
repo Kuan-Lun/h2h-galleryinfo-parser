9f17f20f095bbc3b8c8eeff6b87bf946ab40f556
Kuan-Lun Wang <kuan-lun@klwang.tw>
2024-10-15 10:35:54 +0800

    修正類別屬相不正確判斷型態的問題。

--- src/galleryinfo_parser/__init__.pyi
+++ src/galleryinfo_parser/__init__.pyi
@@ -1,4 +1,20 @@
+from typing import Any
+
 class GalleryInfoParser:
+    __attrs__: Any
+    gallery_folder: str
+    gallery_name: str
+    gid: int
+    files_path: str
+    modified_time: str
+    title: str
+    upload_time: str
+    galleries_comments: str
+    upload_account: str
+    download_time: str
+    tags: list[tuple[str, str]]
+    _pages: int
+
     def __init__(
         self,
         gallery_folder: str,
@@ -13,6 +29,7 @@ class GalleryInfoParser:
         download_time: str,
         tags: list[tuple[str, str]],
     ) -> None: ...
+    @property
     def pages(self) -> int: ...
     def __repr__(self) -> str: ...
     def __str__(self) -> str: ...
