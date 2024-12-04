76dfc25260d7e031e37916358b1c3bec54937928
Kuan-Lun Wang <kuan-lun@klwang.tw>
2024-12-04 10:02:11 +0800

    更改 API 介面。

--- src/galleryinfo_parser/gallery_url_parser.py
+++ src/galleryinfo_parser/gallery_url_parser.py
@@ -6,49 +6,24 @@ import re
 
 class GalleryURLParser:
     """
-    A parser class for extracting gallery information from URLs.
+    A parser for extracting gallery information from URLs of exhentai.org and e-hentai.org.
     Attributes:
-        gid (int): The gallery ID.
-        url (str): The gallery URL.
-        url_key (str): The key associated with the gallery URL.
+        gid (int): The gallery ID extracted from the URL.
+        url (str): The original URL provided.
+        url_key (str): The URL key extracted from the URL.
     Methods:
-        __init__(): Initializes the GalleryURLParser with default values.
-        gid: Property to get or set the gallery ID.
-        url: Property to get or set the gallery URL. The URL must be from exhentai.org or e-hentai.org.
-    Examples:
-        >>> parser = GalleryURLParser()
-        >>> parser.url = "https://exhentai.org/g/1234567/abcdefg"
-        >>> parser.gid
-        1234567
-        >>> parser.url_key
-        "abcdefg"
+        __init__(url: str) -> None:
+            Initializes the GalleryURLParser with the provided URL.
+            Raises a ValueError if the URL is empty or not a valid gallery URL.
     """
 
     __slots__ = ["gid", "url", "url_key"]
 
-    def __init__(self) -> None:
-        self.gid = 0
-        self.url = ""
-        self.url_key = ""
-
-    @property
-    def gid(self) -> int:
-        if self.gid == 0:
-            raise ValueError("GID is not set.")
-        return self.gid
-
-    @gid.setter
-    def gid(self, gid: int) -> None:
-        self.gid = gid
-
-    @property
-    def url(self) -> str:
-        return self.url
-
-    @url.setter
-    def url(self, url: str) -> None:
-        if url != "":
-            if ("exhentai.org" not in self.url) and ("e-hentai.org" not in self.url):
+    def __init__(self, url: str) -> None:
+        if url == "":
+            raise ValueError("The url cannot be empty.")
+        else:
+            if ("exhentai.org" not in url) and ("e-hentai.org" not in url):
                 raise ValueError("The url is not the gallery's url.")
             self.url = url
 
@@ -65,5 +40,3 @@ class GalleryURLParser:
                 self.url_key = match.group(2)
             else:
                 raise ValueError("The url is not the gallery's url.")
-        else:
-            raise ValueError("The url cannot be empty.")
